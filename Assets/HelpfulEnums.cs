fdbec7fdb43bcf5d2d2b5abf1523c1af55aa80e4
YahtzeeRage <34356761+YahtzeeRage@users.noreply.github.com>
2019-01-01 19:41:28 -0500

    musicStuff

---
+++ Assets/HelpfulEnums.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum pressType //a = outerLeft b = innerLeft c = middle d = innerRight e = outerRight, a combo means both
+{
+    a,
+    b,
+    c,
+    d,
+    e,
+    ab,
+    ac,
+    ad,
+    ae,
+    bc,
+    bd,
+    be,
+    cd,
+    ce,
+    de
+}
+
+public struct noteInfo
+{
+    bool isNote;
+    int noteLength;
+    pressType pT;
+
+}
